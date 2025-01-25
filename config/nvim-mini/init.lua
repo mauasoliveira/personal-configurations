a43f3d88bf4ee360fc56ac0f2e2f2f1f496e2bf4
Mauricio Oliveira <mau.asoliveira@gmail.com>
2025-01-24 21:16:26 -0300

    Customization

--- config/nvim-mini/init.lua
+++ config/nvim-mini/init.lua
@@ -2,15 +2,15 @@
 local path_package = vim.fn.stdpath('data') .. '/site'
 local mini_path = path_package .. '/pack/deps/start/mini.nvim'
 if not vim.loop.fs_stat(mini_path) then
-  vim.cmd('echo "Installing `mini.nvim`" | redraw')
-  local clone_cmd = {
-    'git', 'clone', '--filter=blob:none',
-    -- Uncomment next line to use 'stable' branch
-    -- '--branch', 'stable',
-    'https://github.com/echasnovski/mini.nvim', mini_path
-  }
-  vim.fn.system(clone_cmd)
-  vim.cmd('packadd mini.nvim | helptags ALL')
+    vim.cmd('echo "Installing `mini.nvim`" | redraw')
+    local clone_cmd = {
+        'git', 'clone', '--filter=blob:none',
+        -- Uncomment next line to use 'stable' branch
+        -- '--branch', 'stable',
+        'https://github.com/echasnovski/mini.nvim', mini_path
+    }
+    vim.fn.system(clone_cmd)
+    vim.cmd('packadd mini.nvim | helptags ALL')
 end
 
 -- Set up 'mini.deps' (customize to your liking)
@@ -20,53 +20,229 @@ MiniDeps.add({ name = 'mini.nvim', checkout = 'stable' })
 -- TODO : Organize in folders
 --
 
-local add = MiniDeps.add
+local add, now, later  = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 -- Add to current session (install if absent)
 add({
-  source = 'neovim/nvim-lspconfig',
-  -- Supply dependencies near target plugin
-  depends = { 'williamboman/mason.nvim' },
+    source = 'neovim/nvim-lspconfig',
+    -- Supply dependencies near target plugin
+    depends = { 'williamboman/mason.nvim' },
 })
 
 add({
-  source = 'nvim-treesitter/nvim-treesitter',
-  -- Use 'master' while monitoring updates in 'main'
-  checkout = 'master',
-  monitor = 'main',
-  -- Perform action after every checkout
-  hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+    source = 'nvim-treesitter/nvim-treesitter',
+    -- Use 'master' while monitoring updates in 'main'
+    checkout = 'master',
+    monitor = 'main',
+    -- Perform action after every checkout
+    hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
 })
 -- Possible to immediately execute code which depends on the added plugin
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { 'lua', 'vimdoc', 'python', 'dockerfile', 'markdown', 'gitcommit', 'gitignore', 'make' },
-  highlight = { enable = true },
+    ensure_installed = { 'lua', 'vimdoc', 'python', 'dockerfile', 'markdown', 'gitcommit', 'gitignore', 'make' },
+    highlight = { enable = true },
 })
+add({ source = "catppuccin/nvim", name = "catppuccin" })
 
-local now, later = MiniDeps.now, MiniDeps.later
+
+
+
+-- [[ Configurations ]]
+now(function()
+    vim.g.mapleader = " "
+    vim.o.number = true
+    vim.o.relativenumber = true
+    vim.o.autoindent = true
+    vim.o.shiftwidth = 4
+    vim.o.tabstop = 4
+    vim.o.expandtab = true
+    vim.o.scrolloff = 10
+
+    vim.o.spelllang="pt,en"
+
+    -- TODO Configure vimtags
+
+    vim.cmd('colorscheme catppuccin-mocha')
+end)
+
+
+
+
 
 -- Safely execute immediately
 -- TODO Catpucin schema
 
-now(function() vim.cmd('colorscheme minicyan') end)
-
 now(function() require('mini.starter').setup() end)
-now(function() require('mini.notify').setup() end)
-now(function() require('mini.indentscope').setup() end)
 
-now(function() require('mini.misc').setup() end)
-now(function() require('mini.git').setup() end)
-now(function() require('mini.files').setup() end)
-now(function() require('mini.extra').setup() end)
-
-now(function() require('mini.basics').setup() end)
-now(function() require('mini.pairs').setup() end)
-now(function() require('mini.icons').setup() end)
-now(function() require('mini.statusline').setup() end)
-now(function() require('mini.tabline').setup() end)
-now(function() require('mini.fuzzy').setup() end)
+later(function()
+    require("mini.files").setup({
+        mappings = {
+            close = '<ESC>',
+        },
+        windows = {
+            preview = true,
+            border = "solid",
+            width_preview = 80,
+        },
+    })
+end)
+later(function()
+    require("mini.fuzzy").setup()
+end)
+later(function()
+    require("mini.git").setup()
+end)
 
 -- Safely execute later
 later(function() require('mini.pick').setup() end)
+later(function()
+    require("mini.diff").setup({
+        view = {
+            style = "sign",
+            signs = { add = "█", change = "▒", delete = "" },
+        },
+    })
+end)
+
+later(function() require('mini.basics').setup() end)
+later(function() require('mini.icons').setup() end)
+later(function() require('mini.indentscope').setup() end)
+later(function() require('mini.misc').setup() end)
+
+later(function()
+    --          ┌─────────────────────────────────────────────────────────┐
+    --            We took this from echasnovski's personal configuration
+    --           https://github.com/echasnovski/nvim/blob/master/init.lua
+    --          └─────────────────────────────────────────────────────────┘
+    local filterout_lua_diagnosing = function(notif_arr)
+        local not_diagnosing = function(notif)
+            return not vim.startswith(notif.msg, "lua_ls: Diagnosing")
+        end
+        notif_arr = vim.tbl_filter(not_diagnosing, notif_arr)
+        return MiniNotify.default_sort(notif_arr)
+    end
+    require("mini.notify").setup({
+        content = { sort = filterout_lua_diagnosing },
+        window = { config = { border = "solid" } },
+    })
+    vim.notify = MiniNotify.make_notify()
+end)
+later(function()
+    require("mini.operators").setup()
+end)
+later(function()
+    require("mini.pairs").setup()
+end)
+
+-- // TODO Mini.snippets
+
+later(function() require('mini.statusline').setup() end)
+later(function() require('mini.tabline').setup() end)
 
 
+later(function()
+    local animate = require("mini.animate")
+    animate.setup({
+        scroll = {
+            -- Disable Scroll Animations, as the can interfer with mouse Scrolling
+            enable = false,
+        },
+        cursor = {
+            timing = animate.gen_timing.cubic({ duration = 50, unit = "total" }),
+        },
+    })
+end)
+
+later(function()
+    require("mini.clue").setup({
+        triggers = {
+            -- Leader triggers
+            { mode = "n", keys = "<Leader>" },
+            { mode = "x", keys = "<Leader>" },
+
+            { mode = "n", keys = "\\" },
+
+            -- Built-in completion
+            { mode = "i", keys = "<C-x>" },
+
+            -- `g` key
+            { mode = "n", keys = "g" },
+            { mode = "x", keys = "g" },
+
+            -- Marks
+            { mode = "n", keys = "'" },
+            { mode = "n", keys = "`" },
+            { mode = "x", keys = "'" },
+            { mode = "x", keys = "`" },
+
+            -- Registers
+            { mode = "n", keys = '"' },
+            { mode = "x", keys = '"' },
+            { mode = "i", keys = "<C-r>" },
+            { mode = "c", keys = "<C-r>" },
+
+            -- Window commands
+            { mode = "n", keys = "<C-w>" },
+
+            -- `z` key
+            { mode = "n", keys = "z" },
+            { mode = "x", keys = "z" },
+        },
+
+        clues = {
+            { mode = "n", keys = "<Leader>b", desc = " Buffer" },
+            { mode = "n", keys = "<Leader>f", desc = " Find" },
+            { mode = "n", keys = "<Leader>g", desc = "󰊢 Git" },
+            { mode = "n", keys = "<Leader>i", desc = "󰏪 Insert" },
+            { mode = "n", keys = "<Leader>l", desc = "󰘦 LSP" },
+            { mode = "n", keys = "<Leader>m", desc = " Mini" },
+            { mode = "n", keys = "<Leader>q", desc = " NVim" },
+            { mode = "n", keys = "<Leader>s", desc = "󰆓 Session" },
+            { mode = "n", keys = "<Leader>u", desc = "󰔃 UI" },
+            { mode = "n", keys = "<Leader>w", desc = " Window" },
+            require("mini.clue").gen_clues.g(),
+            require("mini.clue").gen_clues.builtin_completion(),
+            require("mini.clue").gen_clues.marks(),
+            require("mini.clue").gen_clues.registers(),
+            require("mini.clue").gen_clues.windows(),
+            require("mini.clue").gen_clues.z(),
+        },
+        window = {
+            delay = 300,
+        },
+    })
+end)
+
+-- [[ Neovide ]]
+--
+now(function()
+    if vim.g.neovide then
+        vim.g.neovide_scroll_animation_length = 0.1
+        vim.opt.mousescroll = "ver:10,hor:6"
+        vim.opt.linespace = -1
+        vim.g.neovide_theme = "dark"
+
+        vim.g.neovide_floating_shadow = true
+        vim.g.neovide_floating_z_height = 2
+        vim.g.neovide_light_angle_degrees = 45
+        vim.g.neovide_light_radius = 15
+
+        vim.g.neovide_floating_blur_amount_x = 10.0
+        vim.g.neovide_floating_blur_amount_y = 10.0
+
+        vim.o.guicursor =
+        "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait100-blinkoff700-blinkon700-Cursor/lCursor,sm:block-blinkwait0-blinkoff300-blinkon300"
+        vim.g.neovide_cursor_animation_length = 0.03
+        vim.g.neovide_cursor_smooth_blink = true
+        vim.g.neovide_cursor_vfx_mode = "pixiedust"
+    end
+end)
+
+
+
+
+
+
+
+--- Resources 
+---  https://gitlab.com/domsch1988/mvim/-/blob/main/init.lua?ref_type=heads
