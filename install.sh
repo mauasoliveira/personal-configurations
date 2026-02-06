c5b6a8caf7d3e437ca576f954988901b7e169e19
Mauricio Oliveira <mau.asoliveira@gmail.com>
2026-02-06 18:21:05 -0300

    zsh optimizations

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt upgrade -y
 # Neovim Dependencies
 sudo apt-get install ninja-build gettext cmake curl build-essential git
 
-sudo apt install -y ripgrep fzf wget zoxide tmux tig zsh openssh-server fd-find lua5.1 luarocks podman mkcert docker-cli docker-compose libnss3-tools bat
+sudo apt install -y ripgrep fzf wget zoxide tmux tig zsh openssh-server fd-find lua5.1 luarocks podman mkcert docker-cli docker-compose libnss3-tools bat starship
 # python3
 
 git config --global pull.rebase false
@@ -18,14 +18,14 @@ cd ~/Documents/Projects/Opensources/neovim
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 
-# echo "Installing NVM" wget -qO- 
-# https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh 
-# | bash
+echo "Installing MISE"
+sudo install -dm 755 /etc/apt/keyrings
+curl -fSs https://mise.jdx.dev/gpg-key.pub | sudo tee /etc/apt/keyrings/mise-archive-keyring.asc 1> /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.asc] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
 
-# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s 
-# "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s 
-# "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm 
-# nvm install --lts nvm use --lts
+sudo apt update -y
+sudo apt install -y mise
+
 #
 echo "Installing TPM"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
